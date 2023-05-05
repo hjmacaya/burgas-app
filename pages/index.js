ef2a41f9037bfd5b3adbdb5b99277c47036e384d
Hernan Macaya <hjmacaya@uc.cl>
2023-05-04 22:59:45 -0400

    add: char dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
 import Navbar from "../components/navbar"
+import Chart from "chart.js"
 
 export default function Home() {
+
+  // Initialize the mock-up data
+  const data = [
+    { date: "2023-01-05", count: 5 },
+    { date: "2023-02-05", count: 3 },
+    { date: "2023-03-05", count: 6 },
+    { date: "2023-04-05", count: 4 },
+    { date: "2023-05-05", count: 7 }
+  ]
+
+  // Create chart
+  let barChart = () => {
+    let stockChartDiv = document.getElementById("stock-chart")
+    new Chart( document.getElementById("stock-chart"), {
+    type: 'bar',
+    data: {
+      labels: data.map(row => row.date),
+      datasets: [
+        {
+          label: 'Burgers Stocks',
+          data: data.map(row => row.count)
+        }
+      ]
+    }
+  })
+  }
+
   return (
     <div>
       <Navbar />
         <div className="container text-center my-3">
-          <h1>Testing bootstrap</h1>
+          <h1>Our stocks: </h1>
+          <div id="stock-chart"></div>
         </div>
     </div>
   )
